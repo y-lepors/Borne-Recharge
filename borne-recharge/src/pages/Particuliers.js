ede5b477232a6c3ca9915a2bfc35d16d17fae277
yanis <yanis.lepors@gmail.com>
2022-01-10 11:04:36 +0100

    particulier-update

--- borne-recharge/src/pages/Particuliers.js
+++ borne-recharge/src/pages/Particuliers.js
@@ -16,23 +16,45 @@ export default class Particuliers extends Component {
       <div class="div-particuliers">
         <h1 class="titre-particuliers">Particuliers</h1>
 
+        <p class="paragraphe-abo">
+          <u>Nos abonnements :</u>
+          <br/><br/>
+
+          Pour les particuliers Borne Recharge propose un système d'abonnement. Lorsque vous souscrivez à un abonnement 
+          il est alors possible pour vous de charger votre véhicule sur les stations de recharge Borne Recharge. Le prix dépend des fonctionnalités que vous sélectionnez.
+          Il est alors conseillé de choisir les caractéristiques en adéquation avec votre véhicule électrique. <br/>
+          Pour souscrire à un abonnement veuillez vous rendre sur la page <a href="/Abonnements">Abonnements</a>.
+          <br/><br/>
+          <u>Installation de borne de recharge :</u>
+          <br/><br/>
+
+          Pour les particuliers Borne Recharge propose d'installer des bornes de recharges électriques directement chez vous. Chaque caractéristique de la borne vous est directement
+          proposée sur la page <a href="/Bornes">Bornes</a>. Veillez à choisir un accès privé de préférence s'il s'agit d'une installation à domicile. Une fois la réservation faites sur la page Bornes
+          vous serez directement contacté par e-mail et l'ensemble des informations concernant la démarche d'installation vous sera fournie accompagnée d'un devis.
+
+
+
+
+        </p>
+        
+
         <p></p>    
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/>
         
         <h2 class="h2-particuliers">Nos réseaux sociaux</h2>
         <br/>
 
         <div class="social-banner">
 
-          <img
+          <a href="https://www.facebook.com"><img
                 alt=""
                 src={facebook}
                 width="100"
                 height="100"
-                className="image1"
-                
-              />{" "}
+                className="image1" 
+              /></a>
 
+          <a href="mailto: contact@borne-recharge.fr">
           <img
               alt=""
               src={gmail}
@@ -40,7 +62,8 @@ export default class Particuliers extends Component {
               height="100"
               className="image2"
               
-            />{" "}
+            />{" "}</a>
+          <a href="https://www.twitter.com">
           <img
               alt=""
               src={twitter}
@@ -48,7 +71,9 @@ export default class Particuliers extends Component {
               height="100"
               className="image3"
               
-            />{"    "}
+            />{"    "}</a>
+
+          <a href="https://www.instagram.com">
           <img
                 alt=""
                 src={instagram}
@@ -57,6 +82,7 @@ export default class Particuliers extends Component {
                 className="image4"
                 
               />{"    "}
+              </a>
 
         </div>
 
